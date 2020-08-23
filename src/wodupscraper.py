6999216cfb2e57b21caa390f3eb66aab4db73d28
Hasan Nagib <hasannagib@Hasans-MacBook-Air.local>
2020-08-23 18:00:44 -0400

    update

--- src/wodupscraper.py
+++ src/wodupscraper.py
@@ -1,16 +1,20 @@
 import pandas as pd
-import numpy as np
 from selenium import webdriver
 from lxml import html
 import time
-import getpass
 import re
-import matplotlib.pyplot as plt
 
 
 class WodUp:
 
     def __init__(self, email, password, url, chrome_driver_path='../src/chromedriver'):
+        """
+
+        :param email: Email address login for WodUp
+        :param password: WodUp login password
+        :param url: url for profile/user to scrape
+        :param chrome_driver_path: path to chromedriver executable
+        """
         self.url = url
         self.email=email
         self.password=password
@@ -21,17 +25,33 @@ class WodUp:
         self.logs = {}
         
     def login(self):
+        """
+        Sign into WodUp using self.email and self.password
+        """
         time.sleep(2)
         self.browser.find_element_by_xpath("//input[@name='username']").send_keys(self.email)
         self.browser.find_element_by_xpath("//input[@type='password']").send_keys(self.password)
         self.browser.find_element_by_xpath("//button[@type='submit']").click()
 
-    def get_html_tree(self, movement):
+    def get_html_tree(self, movement, wait=1.5):
+        """
+        Get html tree for a given movement
+        :param movement: movement to pull html for
+        :param wait: wait to load page
+        :return:
+        """
         self.browser.get(f'{self.url}/movements/{movement}')
-        time.sleep(1.5)
+        time.sleep(wait)
         return html.fromstring(self.browser.page_source)
         
     def get_log(self, movement):
+        """
+        The xpaths pull data for strengh and metcon workouts in a list.
+
+        :param movement: movement to pull data for
+        :return:
+            Store raw output to self.raw_logs dictionary under the movement key
+        """
         tree = self.get_html_tree(movement)
         df = pd.DataFrame({
             'date':tree.xpath('//span[@class="di dn-ns"]/text()'),
@@ -42,26 +62,62 @@ class WodUp:
         self.raw_logs[movement] = df
     
     def gen_weights_list(self, x):
+        """
+        Take out "lbs" from the end of the string and split using dashes.
+        e.g. [35 - 35 - 50 - 90 lbs] --> [35,35,50,90]
+
+        :param x: String containing weights used in the workout
+        :return:
+            list of weights
+        """
         return x.replace(' lbs', '').split(' – ')
 
     def gen_reps_list(self, x):
+        """
+        Generate reps list. Usually this shows up as "8-8-5-5-3-2-1". However, sometimes this will contain "1 Rep" or
+        "1RM" to indicate a one rep max workout. Sometimes it might also be present in "Reps x Sets" format. e.g. 3 x 5.
+        In that scenario convert it to "3-3-3-3-3" first and then split by '-' to get [3,3,3,3,3].
+
+        :param x:
+        :return:
+            list of reps
+        """
         x = x.strip()
-        if re.match('\d\sx\s\d', x):
-            x = '-'.join(int(x[0])*[x[-1]])
+        if re.match('\d+\sx\s\d+', x):
+            x = '-'.join(int(x[0])*[x[-1].strip()])
         elif ('1 Rep' in x)|('1RM' in x):
             x = '1'
         return x.split('-')
     
     def clean_date(self, x):
+        """
+        Date data comes in as Day, Month formant. e.g. "18 Aug". Yesterday's workouts are logged as "Yesterday".
+        Replaces this with yesterday's date and adds current year to all date. Note that the year in the outout of this
+        function is not correct as the current year is applied to all dates. This is fixed by fix_date_year() function.
+
+        :param x: html scraped date strings. e.g "18 Aug"
+        :return:
+            DateTime object corresponding to x
+        """
         if x=='Yesterday':
             out = (pd.Timestamp.today() - pd.DateOffset(days=1)).strftime('%d %b') 
         else:
             out = x
             
-        out = pd.to_datetime(out+'2020')
+        out = pd.to_datetime(out+str(pd.Timestamp.today().year))
         return out
     
     def fix_date_year(self, df):
+        """
+        The html scraped dates come in descending order without year. Every time the month value increases,
+        it indicates a year change. e.g. months: 2, 1, 12  means row corresponding to month 12 is December of last year.
+        Use this increase to indicate rows where a year adjustment needs to be applied. Note: This method will not work
+        if data is too sparse.
+
+        :param df: dataframe with data as DateTime objects
+        :return:
+            df with year correction
+        """
         df.loc[:,'month'] = df['date'].dt.month
         df.loc[:,'month-1'] = df['month'].shift()
         df.loc[:,'offset'] = (df['month-1'] < df['month']).cumsum().apply(lambda x: pd.DateOffset(years=x))
@@ -69,9 +125,19 @@ class WodUp:
         return df
     
     def equalize_reps_and_weights(self, df):
+        """
+        Check to see if reps and weights lists are of euqal lengths. If not, adjust them to be equal. There are several
+        reasons why they may not be equal:
+            - 1 rep max workout
+            - Did extra work beyond workout plan --> len(weights_list) > len(reps_list)
+            - Didn't complete all sets --> len(weights_list) < len(reps_list)
+
+        :param df: DataFrame with reps_list and weights_list columns
+        :return:
+            DataFrame with reps_list and weights_list that are of equal lengths
+        """
         # Convert un-equal length lists into strings
         for i in df.iterrows():
-            # explode 1 RM day reps
             if len(i[1]['reps_list']) == 1:
                 df.loc[i[0],'reps_list'] = '-'.join(i[1]['reps_list']*len(i[1]['weights_list']))
             elif len(i[1]['reps_list']) < len(i[1]['weights_list']):
@@ -84,6 +150,13 @@ class WodUp:
         return df
     
     def clean_rep_list(self, df):
+        """
+        Take max of clustered reps in clusters. e.g. "4.2" should be counted as 4 reps. Impute missing or empty data
+        with 0
+
+        :param df: DataFrame with reps_list
+        :return: DataFrame with cleaned up reps_list
+        """
         for i in df.iterrows():
             for idx, r in enumerate(i[1]['reps_list']):
                 # For clusters, take first rep
@@ -94,6 +167,13 @@ class WodUp:
         return df
     
     def clean_weight_list(self, df):
+        """
+        Sometimes reps data is embedded in weights items. e.g. "25 - 5x35 - 3x50 lbs". Here the rep data for 25 lbs
+        will be in reps_list, but for 35 and 50lbs, the reps in reps_list need to be overwritten by 5 and 3 respectively.
+
+        :param df: DataFrame with weights_list
+        :return: DataFrame with cleaned up weights_list
+        """
         for i in df.iterrows():
             # If rep data is available in weights as r x w, overwrite reps data with r and remove r from weights
             for idx, w in enumerate(i[1]['weights_list']):
@@ -105,7 +185,12 @@ class WodUp:
         return df
     
     def clean_log(self, movement):
-            
+        """
+        Add cleaned up version of self.raw_logs[movement] to self.logs[movement]
+
+        :param movement: movement string. e.g. "back-squat"
+        :return: Cleaned up version of self.raw_logs[movement] that's stored in self.logs[movement]
+        """
         movement_name = ' '.join([i.capitalize() for i in movement.split('-')])
         #print(f'cleaning {movement_name} data...')
 
@@ -133,12 +218,23 @@ class WodUp:
         
         clean_log_cols = ['date', 'reps', 'weights', 'reps_list', 'weights_list']
         self.logs[movement] = df[clean_log_cols]
-        
+
+        return df[clean_log_cols]
+
     def clean_all_logs(self):
+        """
+        Iterate through all movements found in self.raw_logs and clean the logs and populate self.logs
+        """
         for movement in self.raw_logs.keys():
             self.clean_log(movement)
             
     def gen_movement_hist(self, movement):
+        """
+        Explodes the self.logs[movement] DataFrame. This view is useful for generating lift charts
+
+        :param movement: movement string. e.g. "front-squat"
+        :return: Exploded self.logs[movement] DataFrame
+        """
         df = self.logs[movement].copy()
         df_w = df.explode('weights_list').reset_index().drop(columns=['index']).reset_index()[['index', 'date', 'weights_list']]
         df_r = df.explode('reps_list').reset_index().drop(columns=['index']).reset_index()[['index', 'reps_list']]
